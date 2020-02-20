eef41e8eeb3e3b77c56d9f703a29c9c1c1689719
Colin Mulligan <colin.mulligan@pomona.edu>
2020-02-19 17:13:38 -0800

    first edits on word_ladder.py

--- word_ladder.py
+++ word_ladder.py
@@ -28,6 +28,24 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     Whenever it is impossible to generate a word ladder between the two words,
     the function returns `None`.
     '''
+    from collections import deque
+    import copy
+    S = []
+    S.append(start_word)
+    Q = deque()
+    Q.append(S)
+
+    While len(Q) != 0:
+        Q.pop()
+        for word in dictionary_file:
+            if _adjacent(word, S[-1]):
+                if word == end_word:
+                    S.append(word)
+                    return S
+                Scopy = copy.deepcopy(S)
+                Scopy.append(word)
+                Q.append(Scopy)
+                dictionary_file.pop(word)
 
 
 def verify_word_ladder(ladder):
@@ -35,6 +53,14 @@ def verify_word_ladder(ladder):
     Returns True if each entry of the input list is adjacent to its neighbors;
     otherwise returns False.
     '''
+    if len(ladder) <= 1:
+        return False
+    for k in range(len(ladder)-1):
+        if not _adjacent(ladder[k], ladder[k+1]):
+            return False
+    return True
+        
+        
 
 
 def _adjacent(word1, word2):
@@ -47,3 +73,13 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
+    if len(word1) != len(word2):
+        return False
+    count = 0
+    for j in range(len(word1)):
+        if word1[j] != word2[j]:
+            count += 1
+    if count == 1:
+        return True
+    else:
+        return False
